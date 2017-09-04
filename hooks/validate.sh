f13d402bdc7806e5254c17055e1f965bbbffce69
Cyrille Gindreau <cyrillegin@gmail.com>
2017-09-04 16:34:18 -0600

    updating documentation

--- hooks/validate.sh
+++ hooks/validate.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Pico map style guidline enforcement should be added here.
-# TODO: Setup up git hooks to run validation on pre commit
-#       Run install.sh to set it up.
+# Run install.sh to set it up.
 
 #run python validator
 cd ./python/
