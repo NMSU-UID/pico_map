171dbbaa175e94d867690029e828df1ae8033fee
Cyrille Gindreau <cyrillegin@gmail.com>
2017-09-12 09:26:03 -0600

    splitting setup and capture into seperate functions and added main

--- python/imageCapture.py
+++ python/imageCapture.py
@@ -3,7 +3,7 @@ import pygame.camera
 import logging
 
 
-def recordImages():
+def SetupCam():
     logging.info('Starting image record.')
     pygame.init()
     pygame.camera.init()
@@ -11,17 +11,19 @@ def recordImages():
     try:
         cam = pygame.camera.Camera("/dev/video0", (640, 480))
         cam.start()
+        return cam
     except Exception, e:
         print "Error connecting to camera."
-        print e
+        raise Exception(e)
 
+
+def CaptureImage(cam):
     print "Getting image."
     try:
         image = cam.get_image()
     except Exception, e:
         print "Error getting image"
-        print e
-        return
+        raise Exception(e)
 
     logging.info("Saving image.")
     try:
@@ -29,3 +31,19 @@ def recordImages():
     except Exception, e:
         print "Error saving image:"
         print e
+        raise Exception(e)
+
+
+if __name__ == "__main__":
+    print "Setting up."
+    try:
+        cam = SetupCam()
+    except Exception, e:
+        print "Error setting up the camera."
+        exit()
+    try:
+        CaptureImage(cam)
+    except Exception, e:
+        print "Error capturing image."
+        print e
+        exit()
