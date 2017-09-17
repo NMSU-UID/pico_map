d2a56d84601c6e5d0e55eeb5373a2bd9cd5c437c
Cyrille Gindreau <cyrillegin@gmail.com>
2017-09-17 12:17:57 -0600

    documentation

--- Pico map/Assets/Scripts/ImageCapture.cs
+++ Pico map/Assets/Scripts/ImageCapture.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ImageCapture : MonoBehaviour {
+    /*
+        Camera handler.
+        Finds a connected webcam and pipes its image to a
+        texture.
 
+        Public parameters:
+        imgCam - the texture to render the camera image to.
+        showImage - Should the image be rendered
+            primarily useful for debugging, deving
+
+        Public functions:
+        Show()
+        Takes a boolean, sets the camera to show/hide.
+
+        GetTexture()
+        Returns the camera texture.
+
+     */
     private WebCamTexture webCamTexture;
     public RawImage imgCam;
-    public bool ShowImage = true;
+    public bool showImage = true;
 
     public void Start() {
         InitCam();
-        Show(ShowImage);
+        Show(showImage);
     }
 
-	public WebCamTexture GetTexture () {
-		return webCamTexture;
-	}
+    public WebCamTexture GetTexture () {
+        return webCamTexture;
+    }
 
     public void Show(bool bShow) {
         gameObject.SetActive(bShow);
