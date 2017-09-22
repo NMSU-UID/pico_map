2bb87027a33677fd0792ede2d29d0777059184eb
Cyrille Gindreau <cyrillegin@gmail.com>
2017-09-21 19:07:53 -0600

    lint

--- Pico map/Assets/Scripts/Vision/GestureCreator.cs
+++ Pico map/Assets/Scripts/Vision/GestureCreator.cs
@@ -4,68 +4,68 @@ using UnityEngine;
 
 public class GestureCreator : MonoBehaviour {
 
-	public ImageCapture imageCapture;
-	public GameObject myIcon;
-	public Color32 targetColor;
-	public float colorRange;
-	// in s
-	public float processRate;
+    public ImageCapture imageCapture;
+    public GameObject myIcon;
+    public Color32 targetColor;
+    public float colorRange;
+    // in s
+    public float processRate;
 
-	private Color32[] lastFrame;
-	private float timer = 0;
+    private Color32[] lastFrame;
+    private float timer = 0;
 
-	void Update () {
-		timer += Time.deltaTime;
-		if (timer > processRate) {
-			lastFrame = imageCapture.GetColor();
-			ProcessImage();
-			timer = 0;
-		}
-	}
+    void Update () {
+        timer += Time.deltaTime;
+        if (timer > processRate) {
+            lastFrame = imageCapture.GetColor();
+            ProcessImage();
+            timer = 0;
+        }
+    }
 
-	void ProcessImage() {
-		for(int i =0; i < lastFrame.Length; i++){
-			if (i > 1000) {
-				break;
-			}
-			if (inRange (lastFrame[i], targetColor)) {
-				print("in range: " + i);
-				print(lastFrame[i] + "  " + targetColor);
-				myIcon.transform.position = ClampCursor(myIcon.transform.position, GetPos (i));
-				break;
-			}
-		}
-	}
+    void ProcessImage() {
+        for(int i = 0; i < lastFrame.Length; i++) {
+            if (i > 1000) {
+                break;
+            }
+            if (inRange (lastFrame[i], targetColor)) {
+                print("in range: " + i);
+                print(lastFrame[i] + "  " + targetColor);
+                myIcon.transform.position = ClampCursor(myIcon.transform.position, GetPos (i));
+                break;
+            }
+        }
+    }
 
 
-	bool inRange(Color input, Color targetColor){
-		bool redTru = false;
-		bool blueTru = false;
-		bool greenTru = false;
-		if (Mathf.Abs(input[0] - targetColor[0]) < colorRange) {
-			redTru = true;
-		}
-		if (Mathf.Abs(input[1] - targetColor[1]) < colorRange) {
-			blueTru = true;
-		}
-		if (Mathf.Abs(input[2] - targetColor[2]) < colorRange) {
-			greenTru = true;
-		}
-		if (redTru && blueTru && greenTru) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    bool inRange(Color input, Color targetColor){
+        bool redTru = false;
+        bool blueTru = false;
+        bool greenTru = false;
+        if (Mathf.Abs(input[0] - targetColor[0]) < colorRange) {
+            redTru = true;
+        }
+        if (Mathf.Abs(input[1] - targetColor[1]) < colorRange) {
+            blueTru = true;
+        }
+        if (Mathf.Abs(input[2] - targetColor[2]) < colorRange) {
+            greenTru = true;
+        }
+        if (redTru && blueTru && greenTru) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	Vector3 ClampCursor(Vector3 start, Vector3 newP){
-		return new Vector3(Mathf.Clamp(newP.x, start.x-2, start.x+2), Mathf.Clamp(newP.y, start.y-2, start.y+2), 0);
-	}
+    Vector3 ClampCursor(Vector3 start, Vector3 newP){
+        return new Vector3(Mathf.Clamp(newP.x, start.x - 2, start.x + 2), Mathf.Clamp(newP.y, start.y - 2, start.y + 2), 0);
+    }
 
-	Vector3 GetPos(int i){
-		float x = (i % 1280);
-		float y = Mathf.Floor (i / 640);
-		Vector3 final = new Vector3 (x, y, 0);
-		return final;
-	}
+    Vector3 GetPos(int i){
+        float x = (i % 1280);
+        float y = Mathf.Floor (i / 640);
+        Vector3 final = new Vector3 (x, y, 0);
+        return final;
+    }
 }
--- Pico map/Assets/Scripts/Vision/ImageCapture.cs
+++ Pico map/Assets/Scripts/Vision/ImageCapture.cs
@@ -18,9 +18,6 @@ public class ImageCapture : MonoBehaviour {
         Show()
         Takes a boolean, sets the camera to show/hide.
 
-        GetTexture()
-        Returns the camera texture.
-
      */
     private WebCamTexture webCamTexture;
     public RawImage imgCam;
@@ -33,29 +30,18 @@ public class ImageCapture : MonoBehaviour {
         Show(showImage);
     }
 
-    public Color32[] GetColor () {
-        print("Height: " + webCamTexture.height + " Width: " + webCamTexture.width);
-        Color32[] raw = webCamTexture.GetPixels32(data);
-        Color32[] pass = PoolColors(raw, 1280, 720);
-        return pass;
-    }
+    // This should become a cooroutine and handle map placement/scaling.
+    void InitCam() {
+        webCamTexture = new WebCamTexture();
+        webCamTexture.requestedFPS = 1;
+        webCamTexture.requestedWidth = 1280;
+        webCamTexture.requestedHeight = 720;
+        webCamTexture.Play();
+        // this should be set using webCamTexture.width/height but it needs to wait
+        // until initialization is done.
+        data = new Color32[1280 * 720];
 
-    public Color32[] PoolColors (Color32[] startData, int width, int height) {
-        Color32[] resultColor = new Color32[(width/3) * (height/3)];
-        int counter = 0;
-        for(int i = 1; i < width-1; i += 3) {
-            for(int j= 1; j < height-1; j += 3) {
-                float newRed = (startData[i*j].r + startData[i*j + 1].r + startData[i*j - 1].r + startData[(i-1)*j].r + startData[(i + 1)*j].r) / 5;
-                float newGreen = (startData[i*j].g + startData[i*j + 1].g + startData[i*j - 1].g + startData[(i-1)*j].g + startData[(i + 1)*j].g) / 5;
-                float newBlue = (startData[i*j].b + startData[i*j + 1].b + startData[i*j - 1].b + startData[(i-1)*j].b + startData[(i + 1)*j].b) / 5;
-
-                resultColor[counter] = new Color32((byte) newRed, (byte) newGreen, (byte) newBlue, 1);
-                counter ++;
-            }
-        }
-        print("done pooling, first result: " + resultColor[0] + " from " + startData[width + 2]);
-        print("Array size start: " + (width * height) + " to " + counter);
-        return resultColor;
+        imgCam.texture = webCamTexture;
     }
 
     public void Show(bool bShow) {
@@ -72,17 +58,30 @@ public class ImageCapture : MonoBehaviour {
         }
     }
 
-    // This should become a cooroutine and handle map placement/scaling.
-    void InitCam() {
-        webCamTexture = new WebCamTexture();
-        webCamTexture.requestedFPS = 1;
-        webCamTexture.requestedWidth = 1280;
-        webCamTexture.requestedHeight = 720;
-        webCamTexture.Play();
-        // this should be set using webCamTexture.width/height but it needs to wait
-        // until initialization is done.
-        data = new Color32[1280 * 720];
-
-        imgCam.texture = webCamTexture;
+    public Color32[] GetColor () {
+        print("Height: " + webCamTexture.height + " Width: " + webCamTexture.width);
+        Color32[] raw = webCamTexture.GetPixels32(data);
+        Color32[] pass = PoolColors(raw, 1280, 720);
+        return pass;
     }
+
+    public Color32[] PoolColors (Color32[] startData, int width, int height) {
+        Color32[] resultColor = new Color32[(width / 3) * (height / 3)];
+        int counter = 0;
+        for(int i = 1; i < width - 1; i += 3) {
+            for(int j = 1; j < height - 1; j += 3) {
+                float newRed = (startData[i * j].r + startData[i * j + 1].r + startData[i * j - 1].r + startData[(i - 1) * j].r + startData[(i + 1) * j].r) / 5;
+                float newGreen = (startData[i * j].g + startData[i * j + 1].g + startData[i * j - 1].g + startData[(i - 1) * j].g + startData[(i + 1) * j].g) / 5;
+                float newBlue = (startData[i * j].b + startData[i * j + 1].b + startData[i * j - 1].b + startData[(i - 1) * j].b + startData[(i + 1) * j].b) / 5;
+
+                resultColor[counter] = new Color32((byte) newRed, (byte) newGreen, (byte) newBlue, 1);
+                counter++;
+            }
+        }
+        print("done pooling, first result: " + resultColor[0] + " from " + startData[width + 2]);
+        print("Array size start: " + (width * height) + " to " + counter);
+        return resultColor;
+    }
+
+
 }
