a38e3465588bb8ef032c80363a87c391ec846851
Randall Woodall <randalljwoodall@gmail.com>
2017-10-20 21:32:14 -0600

    intensity
    Basic change in intensity from a slider.

---
+++ Pico map/Assets/Scripts/LightControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour {
+    //changeIntensity controls intensity of the light
+    public void changeIntensity(float val)
+    {
+        Light dirLight = GetComponent<Light>();
+        dirLight.intensity = val;
+    }
+}
