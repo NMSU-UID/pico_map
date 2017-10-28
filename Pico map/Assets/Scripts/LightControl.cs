776fa73cf24125f2404e6eecf59df08cfaae0350
Randall Woodall <randalljwoodall@gmail.com>
2017-10-28 17:32:45 -0600

    Light Control
    Changed the lighting scene and added a slider to control angle.  Next
    step before merge to master includes combining both sliders into one fed
    from the same slider.

--- Pico map/Assets/Scripts/LightControl.cs
+++ Pico map/Assets/Scripts/LightControl.cs
@@ -1,12 +1,27 @@
-﻿using System.Collections;
+﻿/* Randall Woodall
+ * October 2017
+ * LightControl.cs
+ * Purpose: Prototype to control light.
+ * TODO: link the two functions together, 
+ *       make them private and function off one input
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LightControl : MonoBehaviour {
     //changeIntensity controls intensity of the light
-    public void changeIntensity(float val)
-    {
+    public void changeIntensity(float val) {
         Light dirLight = GetComponent<Light>();
         dirLight.intensity = val;
-    }
+    }//End changeIntensity
+
+    //changeAngle changes the angle of the light
+    public void changeAngle(float ammt) {
+        Light dirLight = GetComponent<Light>();
+	dirLight.transform.rotation = Quaternion.Euler(0,
+		ammt * 160 - 80,
+		0);
+    }//End changeAngle
 }
