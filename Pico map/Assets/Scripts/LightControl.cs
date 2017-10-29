87b2299b616eec2865948318f2b9b24630f07916
Randall Woodall <randalljwoodall@gmail.com>
2017-10-28 19:44:06 -0600

    Color Slider
    Added component: the color slider.  The controls can be left as they are
    for a lo-res presentation, with the ultimate goal of combining them all
    to make a dynamic system from a single slider.

--- Pico map/Assets/Scripts/LightControl.cs
+++ Pico map/Assets/Scripts/LightControl.cs
@@ -2,7 +2,7 @@
  * October 2017
  * LightControl.cs
  * Purpose: Prototype to control light.
- * TODO: link the two functions together, 
+ * TODO: link the three functions together, 
  *       make them private and function off one input
  */
 
@@ -16,11 +16,23 @@ public class LightControl : MonoBehaviour {
 
     //changeIntensity controls intensity of the light
     public void changeIntensity(float val) {
-        dirLight.intensity = val;
+        dirLight.intensity = val / 10;
     }//End changeIntensity
 
     //changeAngle changes the angle of the light
-    public void changeAngle(float ammt) {
-        dirLight.transform.rotation = Quaternion.Euler(0, ammt * 160 - 80, 0);
+    public void changeAngle(float val) {
+        dirLight.transform.rotation = Quaternion.Euler(0, (val/12) * 160 - 80, 0);
     }//End changeAngle
+    
+    //changeColor changes the color of the light
+    //(Ranging Yellow to Cyan) Although color uses 0-1, we're using 0-510 
+    //which is really (0-255), (0-255) first set in Blue, second in Red
+    public void changeColor(float val) {
+        if(val <= 255)
+            dirLight.color = new Color(1, 1, val/255, 1);
+        else if (val != 510)
+            dirLight.color = new Color(1-(val%255)/255, 1, 1, 1);
+        else
+            dirLight.color = new Color(0,1,1,1);
+    }//End changeColor
 }
